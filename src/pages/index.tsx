81d86bf9556c5c59c24eabd4c143fe130b9b4c21
Chrisando Pramudhita <chrsep@protonmail.com>
2020-10-10 20:47:13 +0700

    stop using dummy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -52,7 +52,7 @@ const Home: FC<Props> = ({ events }) => {
 export async function getStaticProps() {
   return {
     props: {
-      events: [],
+      events: await listEvents(),
     },
     // we will attempt to re-generate the page:
     // - when a request comes in
