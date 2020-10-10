f1aa5048641becc552bb44c1ad4da2f601481213
Chrisando Pramudhita <chrsep@protonmail.com>
2020-10-10 11:17:35 +0700

    fix build problem.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import { FC } from "react"
 import { AppPropsType } from "next/dist/next-server/lib/utils"
-import "../global.css"
 
 const MyApp: FC<AppPropsType> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
