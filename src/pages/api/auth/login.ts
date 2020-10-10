4fbec6adf800463dca839b5dc94d60c9419fc59c
Chrisando Pramudhita <chrsep@protonmail.com>
2020-10-10 16:17:57 +0700

    add auth0 fix build problems.

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -1,7 +1,19 @@
 import { NextApiHandler } from "next"
+import auth0 from "../../utils/auth0"
 
-const login: NextApiHandler = (req, res) => {
-  res.end()
+const login: NextApiHandler = async (req, res) => {
+  try {
+    const session = await auth0.getSession(req)
+    if (session) {
+      res.setHeader("Location", "/")
+      res.status(302).end()
+      return
+    }
+    await auth0.handleLogin(req, res)
+  } catch (error) {
+    console.error(error)
+    res.status(error.status || 400).end(error.message)
+  }
 }
 
 export default login
