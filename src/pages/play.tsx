3240622d1bc45d49ecc6d9d39e268e8e54004f39
Chrisando Pramudhita <chrsep@protonmail.com>
2020-10-11 23:46:56 +0700

    add proper video playback.

--- src/pages/play.tsx
+++ src/pages/play.tsx
@@ -1,29 +1,27 @@
 /** @jsx jsx */
-import React, { useEffect, useRef } from "react"
-import { jsx, Box, Heading } from "theme-ui"
+import React, { useEffect, useRef, useState } from "react"
+import { jsx, Box, Heading, Button, Flex } from "theme-ui"
 import Hls from "hls.js"
 import { useQueryString } from "../hooks/useQueryString"
 import useGetPlaybackId from "../hooks/useGetPlaybackId"
 
 const Play = () => {
+  const [isLoded, setIsLoaded] = useState(false)
   const ref = useRef<HTMLVideoElement>(null)
   const id = useQueryString("eventId")
   const playbackId = useGetPlaybackId(parseInt(id, 10))
 
-  const playbackUrl = `https://stream.mux.com/${playbackId.data?.playbackId}.m3u8`
-
   useEffect(() => {
     console.log("tes")
-    if ((playbackId.data?.playbackId, ref.current)) {
-      console.log("test")
+    if (!isLoded && playbackId.data?.playbackId !== undefined && ref.current) {
+      const playbackUrl = `https://stream.mux.com/${playbackId.data?.playbackId}.m3u8`
+      console.log(playbackUrl)
       const hls = new Hls()
       hls.loadSource(playbackUrl)
       hls.attachMedia(ref.current)
-      hls.on(Hls.Events.MANIFEST_PARSED, function () {
-        ref.current?.play()
-      })
+      setIsLoaded(true)
     }
-  }, [playbackUrl, playbackId.data])
+  }, [playbackId.data])
 
   if (playbackId.isLoading) {
     return (
@@ -35,7 +33,12 @@ const Play = () => {
 
   return (
     <Box>
-      <video id="player" ref={ref} />
+      <video
+        id="player"
+        ref={ref}
+        sx={{ width: "100%", backgroundColor: "black" }}
+        controls
+      />
     </Box>
   )
 }
